e40f8b6c0639236783f64c74210e9eb7620e2857
ALAN EVANDRO BARBOZA <35473833+alanebarboza@users.noreply.github.com>
2024-02-15 12:54:47 -0300

    Initial Commit
    Initial Commit

---
+++ EntityDbExtensions/EntityDbExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityDbExtensions
+{
+    public static class EntityDbExtensions
+    {
+        public async static Task UpdateAndHandleDeletedChildren(this DbContext context, object entityUpdated, object entityFromDb, bool execUpdate = true)
+        {
+            var child_entityUpdated = entityUpdated.GetType()
+                                                   .GetProperties()
+                                                   .FirstOrDefault(p => p.GetGetMethod().IsVirtual && p.PropertyType.IsGenericType);
+
+            if (child_entityUpdated is not null)
+            {
+                var value_entityUpdated = (System.Collections.IEnumerable)child_entityUpdated.GetValue(entityUpdated);
+                var value_entityFromDb = (System.Collections.IEnumerable)child_entityUpdated.GetValue(entityFromDb);
+
+                var deletedChildren = value_entityFromDb.Cast<object>()
+                                                        .Where(childFromDb => !value_entityUpdated.Cast<object>()
+                                                                                                  .Any(childUpdated => Compare(childFromDb, childUpdated)))
+                                                        .ToList();
+                if (deletedChildren.Any())
+                {
+                    foreach (var deletedChild in deletedChildren)
+                        context.Entry(deletedChild).State = EntityState.Deleted;
+                }
+
+                var nonDeletedChildrenFromDb = value_entityFromDb.Cast<object>()
+                                                                 .Where(childFromDb => !deletedChildren.Cast<object>()
+                                                                                                       .Any(deletedChild => Compare(childFromDb, deletedChild)))
+                                                                 .ToList();
+
+                foreach (var nonDeletedChildFromDb in nonDeletedChildrenFromDb)
+                {
+                    var childUpdated = ((System.Collections.IEnumerable)value_entityUpdated).Cast<object>()
+                                                                                            .FirstOrDefault(chilUpdated => Compare(chilUpdated, nonDeletedChildFromDb));
+                    if (childUpdated is not null)
+                        await context.UpdateAndHandleDeletedChildren(childUpdated, nonDeletedChildFromDb, execUpdate);
+                }
+
+                if (execUpdate)
+                    context.Update(entityUpdated);
+            }
+        }
+
+        private static bool Compare(object childFromDb, object childUpdated) =>  GetPropertyValue(childFromDb, "Id") == GetPropertyValue(childUpdated, "Id");
+
+        private static string GetPropertyValue(object objeto, string nomePropriedade) => Convert.ToString(objeto.GetType().GetProperty(nomePropriedade)?.GetValue(objeto));
+    }
+}
---
+++ README.md
@@ -0,0 +1,65 @@
+## EntityDbExtensions
+### Simplify Entity Framework Delete Children Operations
+
+
+## Overview
+
+Tired of manual and repetitive code when it comes to deleting child and subsequent objects with validations and nested loops? 
+The EntityDbExtensions library simplifies the process for you. 
+By simply invoking the method UpdateAndHandleDeletedChildren in the .Update of the root class, it will automatically handle the recursive deletion of all child, grandchild, and subsequent objects that have been removed from your root class.
+
+The method will inspect classes containing a virtual child property with a List type, such as IEnumerable<YourClass>, ICollection<YourClass> or List<YourClass>. 
+```bash
+ public virtual IEnumerable<YourChild> YourChild { get; private set; }
+```
+It performs a validation check to determine if the object has been deleted, marking it as removed. 
+This marking signals Entity Framework to handle the deletion seamlessly. 
+The validation process is recursive, ensuring that all child and subsequent objects undergo thorough validation for removal.
+
+
+## Features
+
+- **Effortless Database Operations:** Streamline your Entity Framework database operations with simplified methods.
+
+- **Extension Methods for DbContext:** Easily perform common database operations using intuitive extension methods on your DbContexts.
+
+- **Update and Handle Deleted Children:** Handle the validation and removal of all deleted children from your root class with just a single command.
+
+
+## Getting Started
+
+1. **Installation:**
+   - Install the EntityDbExtensions NuGet package in your .NET project.
+
+```bash
+<PackageReference Include="EntityDbExtensions" Version="1.0.1" />
+```
+
+Usage:
+Add the EntityDbExtensions using statement to your code.
+Start using the extension methods on your Entity Framework entities.
+
+```bash
+using EntityDbExtensions;
+
+ public async Task<bool> Update(YourEntity entity)
+ {
+    var entityDb = await Get(entity.Id);
+    if (entityDb == null) throw new InvalidOperationException($"{nameof(YourEntity)} not found.");
+	
+    _yourDbContext.UpdateAndHandleDeletedChildren(entity, entityDb);  // Here the magic happens.
+
+    return await _yourDbContext.SaveChangesAsync() > 0;
+ }
+```
+
+
+#### Contribution
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or create a pull request.
+
+#### License	
+This project is licensed under the MIT License.
+
+
+
+LinkedIn - ( https://www.linkedin.com/in/alan-evandro-barboza/ )
